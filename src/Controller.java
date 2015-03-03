a16173cd20a57387698d1fd7f097c6b961fe5f64
Ivy Xing <mxing@bowdoin.edu>
2015-03-02 22:05:55 -0500

    dance moves 1-20

--- src/Controller.java
+++ src/Controller.java
@@ -17,7 +17,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 		 ArrayList<Move> moves = new ArrayList<Move>();
-		  //for(int i = 0; i < 20;i++ ){
+		  
 
 			  Move move1 = new Move();
 			  moves.add(move1);
@@ -31,7 +31,54 @@ public class Controller {
 			  
 			  Move move4 = new Move();
 			  moves.add(move4);
-		  //}
+			  
+			  Move move5 = new Move();
+			  moves.add(move5);
+			  
+			  Move move6 = new Move();
+			  moves.add(move6);
+			  
+			  Move move7 = new Move();
+			  moves.add(move7);
+			  
+			  Move move8 = new Move();
+			  moves.add(move8);
+			  
+			  Move move9 = new Move();
+			  moves.add(move9);
+			  
+			  Move move10 = new Move();
+			  moves.add(move10);
+			  
+			  Move move11 = new Move();
+			  moves.add(move11);
+			  
+			  Move move12 = new Move();
+			  moves.add(move12);
+			  
+			  Move move13 = new Move();
+			  moves.add(move13);
+			  
+			  Move move14 = new Move();
+			  moves.add(move14);
+			  
+			  Move move15 = new Move();
+			  moves.add(move15);
+			  
+			  Move move16 = new Move();
+			  moves.add(move16);
+			  
+			  Move move17 = new Move();
+			  moves.add(move17);
+			  
+			  Move move18 = new Move();
+			  moves.add(move18);
+			  
+			  Move move19 = new Move();
+			  moves.add(move19);
+			  
+			  Move move20 = new Move();
+			  moves.add(move20);
 
 			int lengthOfDance = 20;
 			int numberOfMoves = 20;
